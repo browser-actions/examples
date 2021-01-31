192c406ac0be3781e1b4514977f46e8443e35c76
Shin'ya Ueoka <ueokande@i-beam.org>
2021-01-31 16:12:55 +0900

    Add microsoft-edge matrix

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['FirefoxHeadless', 'ChromiumHeadless'],
+    browsers: ['FirefoxHeadless', 'ChromiumHeadless', 'Edge'],
     singleRun: true,
     concurrency: Infinity
   })
